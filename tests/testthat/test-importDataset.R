f654e2a4c9100bc26ed15f5407ecbbee5b4d2689
Pedro J. Molina <pjmolina@gmail.com>
2017-05-24 11:32:22 +0200

    Sync tests with changes in the backend.

--- tests/testthat/test-importDataset.R
+++ tests/testthat/test-importDataset.R
@@ -8,16 +8,16 @@ test_that("importDataset Error should fail with errors", {
   expect_equal(TRUE, is.list(out))
   expect_equal(FALSE, out$valid)
   expect_equal(FALSE, out$imported)
-  expect_equal(4, length(out$errors))
+  expect_equal(10, length(out$errors))
   expect_equal(201, out$errors[[1]]$code)
   expect_equal(202, out$errors[[2]]$code)
-  expect_equal(401, out$errors[[3]]$code)
-  expect_equal(401, out$errors[[4]]$code)
-  expect_equal("Invalid year specied: 5000 at line 3", out$errors[[1]]$message)
-  expect_equal("Invalid month specied: 16 at line 5", out$errors[[2]]$message)
-  expect_equal("Unreconized or ambiguous species: 'Andrena sp'. Nearest registered in ITIS is: 'Andrena specularia' at line 2",
+  expect_equal(601, out$errors[[3]]$code)
+  expect_equal(601, out$errors[[4]]$code)
+  expect_equal("Invalid year specified: 5000 at line 3", out$errors[[1]]$message)
+  expect_equal("Invalid month specified: 16 at line 5", out$errors[[2]]$message)
+  expect_equal("Trait name: 'sex' not defined for species: 'Andrena sp' at line 2",
                 out$errors[[3]]$message)
-  expect_equal("Unreconized or ambiguous species: 'Bombus sp'. Nearest registered in ITIS is: 'Bombus sporadicus' at line 4",
+  expect_equal("Trait name: 'sex' not defined for species: 'Andrena barbilabris' at line 3",
                 out$errors[[4]]$message)
 })
 
