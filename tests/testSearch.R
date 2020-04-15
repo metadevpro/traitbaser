976f3020b271b97440ec991058a7f628415c86b6
Angel Gimenez <angel.gimenez@bc3research.org>
2020-04-15 09:06:25 +0200

    Add some notes to testsearch

--- tests/testSearch.R
+++ tests/testSearch.R
@@ -6,6 +6,7 @@ library(traitbaser)
 source("R/auxiliar.R")
 source("tests/search.R")
 source("tests/queryList.R")
-dfOsmiaCaerulescens = search(species="Osmia caerulescens", traits="all")
+cnx <- connect(url = "https://traitbase-dev.herokuapp.com/", "root", "bee4") #demo not working
+dfOsmiaCaerulescens = search(cnx, species="Osmia caerulescens", traits="all")
 
 
